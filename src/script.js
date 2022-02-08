56d7565553a3965e0593e650e6b5a38840235c53
Amit Yadav <amityadav@cedcoss.com>
2022-02-08 12:45:43 +0530

    fixed issues

--- src/script.js
+++ src/script.js
@@ -1,25 +1,42 @@
-document.getElementById("addProduct").onclick= function addProductToCart(){
-    var proId=document.getElementById("productId").value;
-    var proName=document.getElementById("productName").value;
-    var proPrice=document.getElementById("productPrice").value;
-    display(proId,proName,proPrice);
+var products = [];
+document.getElementById("addProduct").onclick=function addToCart() {
+  var proId = document.getElementById("productId").value;
+  var proName = document.getElementById("productName").value;
+  var proPrice = document.getElementById("productPrice").value;
+  var proJson = {
+    id: proId,
+    name: proName,
+    price: proPrice,
+  };
+  if (proId != "" && proName != "" && proPrice != "") {
+    if(!checkProduct(proJson.id)){
+        products.push(proJson);
+    }else{
+        alert("Product "+proJson.id+" Already exist")
+    }
+    display();
+  }
 }
-function display(proId,proName,proPrice){
-    var p={
-            name:proName,
-            id:proId,
-            price:proPrice
-        };
-    document.getElementById("productTable").style.display="block";
-    var row=document.getElementById("productTableBody").insertRow(0);
-    row.insertCell(0).innerHTML=p.id;
-    row.insertCell(1).innerHTML=p.name;
-    row.insertCell(2).innerHTML=p.price;
-
-}
-function isEmpty(productId,productName,productPrice){
-    if(productId.length==0 || productName.length==0 || productPrice.length==0){
-        return true;
+//to check if products already exist in the product array
+function checkProduct(proId) {
+    for (var i = 0; i < products.length; i++) {
+        if (proId == products[i].id) {
+            return true;
+        }
     }
     return false;
+}
+
+//to display the products 
+function display() {
+    var tBody="";
+  for (var i = 0; i< products.length; i++) {
+     tBody+=' <tr>\
+                <td>'+products[i].id+'</td>\
+                <td>'+products[i].name+'</td>\
+                <td>'+products[i].price+'</td>\
+            </tr>';
+  }
+  document.getElementById("productBody").innerHTML=tBody
+  document.getElementById("productTable").style.display = "block";
 }
